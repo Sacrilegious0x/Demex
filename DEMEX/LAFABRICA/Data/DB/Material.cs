6fecfa0273ddea91d805fc7b910d46554e0fbf59
Gabriel Moya <itsgamc@gmail.com>
2025-10-07 19:47:25 -0600

    Mapeo y conexion a la base de datos

---
+++ DEMEX/LAFABRICA/Data/DB/Material.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LAFABRICA.Data.DB;
+
+public partial class Material
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public decimal? PricePurchase { get; set; }
+
+    public string PhotoUrl { get; set; } = null!;
+
+    public int? SupplierId { get; set; }
+
+    public virtual ICollection<Inventory> Inventories { get; set; } = new List<Inventory>();
+
+    public virtual ICollection<MaterialSupplier> MaterialSuppliers { get; set; } = new List<MaterialSupplier>();
+
+    public virtual Supplier? Supplier { get; set; }
+}
