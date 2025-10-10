b63463c0e7e06d2efacc4af2ca2f7f994735d649
Gabriel Moya <itsgamc@gmail.com>
2025-10-10 02:36:44 -0600

    Implementacion formulario

--- DEMEX/LAFABRICA/Data/DB/Client.cs
+++ DEMEX/LAFABRICA/Data/DB/Client.cs
@@ -1,24 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace LAFABRICA.Data.DB;
 
 public partial class Client
 {
     public int Id { get; set; }
-
+    [Required(ErrorMessage = "El nombre es requerido.")]
     public string Name { get; set; } = null!;
-
+    [Required(ErrorMessage = "El contacto es requerido.")]
     public string PhoneNumber { get; set; } = null!;
-
+    [Required(ErrorMessage = "El nombre del encargado es requerido.")]
     public string Manager { get; set; } = null!;
-
+    [Required(ErrorMessage = "El contacto del encargado es requerido.")]
     public string ManagerPhoneNumber { get; set; } = null!;
-
+    [Required(ErrorMessage = "El correo es requerido.")]
     public string Email { get; set; } = null!;
-
+    [Required(ErrorMessage = "La Ubicacion es requerida.")]
     public string Location { get; set; } = null!;
-
+    [Required(ErrorMessage = "Debe especificar una ubicacion.")]
     public string SpecificLocation { get; set; } = null!;
 
     public int? QuantityPurchase { get; set; }
