a0d3e0a0fe5d8cde851d8c31e2c007d75610e686
Angel Barboza <Angelbarbozareyes29@gmail.com>
2025-10-27 16:41:36 -0600

    productos validados y listos

--- DEMEX/LAFABRICA/Data/DB/Product.cs
+++ DEMEX/LAFABRICA/Data/DB/Product.cs
@@ -1,33 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; 
 
 namespace LAFABRICA.Data.DB;
 
 public partial class Product
 {
+    [Required]
     public int Id { get; set; }
 
+    [Required]
+    [StringLength(255)] 
     public string Name { get; set; } = null!;
 
+    [Required]
+    [StringLength(255)] 
     public string Category { get; set; } = null!;
 
+    [Required]
+    [StringLength(255)] 
     public string Description { get; set; } = null!;
 
+    [Required]
     public decimal PriceBase { get; set; }
 
+    [Required]
     public byte IsCustom { get; set; }
 
     public string Complexity { get; set; } = null!;
 
+    [StringLength(255)]
     public string? PhotoUrl { get; set; }
 
+    [Required]
     public byte IsActive { get; set; }
 
+    // --- Relaciones ---
     public virtual ICollection<ProductOrder> ProductOrders { get; set; } = new List<ProductOrder>();
-
     public virtual ICollection<Material> Materials { get; set; } = new List<Material>();
-
     public virtual ICollection<ProductMaterial> ProductMaterials { get; set; } = new List<ProductMaterial>();
     public virtual ICollection<PayEmployeeProduct> PayEmployeeProducts { get; set; } = new List<PayEmployeeProduct>();
-
 }
