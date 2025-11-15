a68067653439bed95a72e023e4b19f76ff096012
Angel Barboza <Angelbarbozareyes29@gmail.com>
2025-11-15 00:14:38 -0600

    sin e lservers

--- DEMEX/LAFABRICA.UI.Test/Components/BaseTestANGEL.cs
+++ DEMEX/LAFABRICA.UI.Test/Components/BaseTestANGEL.cs
@@ -8,20 +8,30 @@ namespace LAFABRICA.UI.Test.Components
     {
         protected readonly IWebDriver _driver;
 
-        // URL de tu IIS (dejar como estaba)
+        // URL de tu IIS
         protected readonly string _appUrl = "http://localhost:5290";
 
         public BaseTestANGEL()
         {
             var options = new EdgeOptions();
-            options.AddArgument("--headless");
+
+            // 👇 MODO HEADLESS ESTABLE PARA CI / JENKINS
+            options.AddArgument("headless=new");  // Reemplaza el viejo "--headless"
             options.AddArgument("--disable-gpu");
             options.AddArgument("--no-sandbox");
+            options.AddArgument("--disable-dev-shm-usage");
+            options.AddArgument("--remote-debugging-port=0");
+            options.AddArgument("--disable-software-rasterizer");
             options.AddArgument("--window-size=1920,1080");
+            options.AddArgument("--disable-extensions");
+            options.AddArgument("--disable-infobars");
 
-            // 🚫 Se eliminó WebDriverManager (esto rompe Jenkins)
-            // ⬇️ Ahora EdgeDriver funciona directo en Jenkins
-            _driver = new EdgeDriver(options);
+            // 👇 IMPORTANTE EN JENKINS: usar driver del PATH o bin carpeta
+            var service = EdgeDriverService.CreateDefaultService();
+            service.EnableVerboseLogging = false;
+            service.HideCommandPromptWindow = true;
+
+            _driver = new EdgeDriver(service, options);
         }
 
         public void Dispose()
