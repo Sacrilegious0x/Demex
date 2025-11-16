d6677aa6fc7d798bb64b36e349a7deba6bd20d5f
Gabriel Moya <itsgamc@gmail.com>
2025-11-16 00:46:25 -0600

    cambio a chrome para jenkins

--- DEMEX/LAFABRICA.UI.Test/Components/BaseTest.cs
+++ DEMEX/LAFABRICA.UI.Test/Components/BaseTest.cs
@@ -3,29 +3,32 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using System;
 
-namespace LAFABRICA.Tests.Components
-{
-    public class BaseTest : IDisposable
+    namespace LAFABRICA.Tests.Components
     {
-        public IWebDriver Driver { get; private set; }
-        public string BaseUrl = "http://localhost:5290";
-        public BaseTest()
+        public class BaseTest : IDisposable
         {
-            var options = new ChromeOptions();
-            //options.AddArguments("--start-maximized"); //tamanho de la ventana en edge
-            // PARA JENKINS LUEGO (invisible)
-            options.AddArgument("--headless=new");
-            options.AddArgument("--no-sandbox");
-            options.AddArgument("--disable-gpu");
-            options.AddArgument("--disable-dev-shm-usage");
+            public IWebDriver Driver { get; private set; }
+            public string BaseUrl = "http://localhost:5290";
+            public BaseTest()
+            {
+                var options = new ChromeOptions();
+                //options.AddArguments("--start-maximized"); //tamanho de la ventana en edge
+                // PARA JENKINS LUEGO (invisible)
+                options.AddArgument("--headless=new");
+                options.AddArgument("--no-sandbox");
+                options.AddArgument("--disable-gpu");
+                options.AddArgument("--disable-dev-shm-usage");
+                options.AddArgument("--window-size=1920,1080"); //importante
+                options.AddArgument("--disable-web-security");
+                options.AddArgument("--allow-running-insecure-content");
 
             Driver = new ChromeDriver(options);
-        }
+            }
 
-        public void Dispose()
-        {
-            Driver.Quit();
-            Driver.Dispose();
+            public void Dispose()
+            {
+                Driver.Quit();
+                Driver.Dispose();
+            }
         }
     }
-}
