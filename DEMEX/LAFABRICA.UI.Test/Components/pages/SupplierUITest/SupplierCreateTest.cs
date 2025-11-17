2f4544b441122cd67172858f303d81dcdb509957
PABLO JOSUE RAMIREZ UGALDE <PABLO.RAMIREZUGALDE@ucr.ac.cr>
2025-11-17 12:05:33 -0600

    cambio telefono para que sirva prueba

--- DEMEX/LAFABRICA.UI.Test/Components/pages/SupplierUITest/SupplierCreateTest.cs
+++ DEMEX/LAFABRICA.UI.Test/Components/pages/SupplierUITest/SupplierCreateTest.cs
@@ -39,7 +39,7 @@ namespace LAFABRICA.UI.Test.Components.pages.SupplierUITest
             // Generar datos únicos para evitar colisiones
             var uniqueName = "ProveedorTest-" + Guid.NewGuid().ToString("N").Substring(0, 8);
             var email = $"{uniqueName}@example.com";
-            var phone = "2094-6776";
+            var phone = "2094-5665";
 
             // Rellenar formulario
             var nameInput = _driver.FindElement(By.Id("supplierName"));
